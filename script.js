f69a3f66c910ace259090c34b4bb11f890ff4df9
Mainulhasan05 <mdrifatbd5@gmail.com>
2021-09-25 23:25:21 +0600

    all labs

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
 function btnClicked(){
-    var x1=document.getElementById("time1").value;
-var y1=document.getElementById("days1").value;
+    let x1=document.getElementById("time1").value;
+let y1=document.getElementById("days1").value;
 
-var x2=document.getElementById("time2").value;
-var y2=document.getElementById("days2").value;
+let x2=document.getElementById("time2").value;
+let y2=document.getElementById("days2").value;
 
-var x3=document.getElementById("time3").value;
-var y3=document.getElementById("days3").value;
+let x3=document.getElementById("time3").value;
+let y3=document.getElementById("days3").value;
 
 
 var obj=[];
@@ -36,17 +36,42 @@ var obj=[];
         document.getElementById("output").innerHTML=conflict;
     }
 }
+/*checkbox indexs 2,5,8  */
+/*var labObj=[];
+labObj[0]={
+    labD:
+}*/
 
-function myFunction() {
-     var checkBox= document.getElementById("flexCheckDefault").checked;
-     console.log(checkBox);
-    if(checkBox){
-        var v=document.getElementById("l1").disabled = false;
-        var v=document.getElementById("l11").disabled = false;
+function myFunction(param) {
+     var whichOne=document.querySelectorAll("input");
+     const labObj=document.querySelectorAll("select");
+     var index=2;
+     var d=2,t=3;
+     if(param==1){
+         index=2;
+         d=2,t=3;
+     }
+     else{
+         for(let x=0; x<param-1; x++){
+             index+=3;
+             d+=4;
+             t+=4;
+             console.log(d);
+             console.log(t);
+         }
+     }
+
+     if(whichOne[index].checked){
+        labObj[d].disabled=false;
+        labObj[t].disabled=false;
     }
     else{
-        var v=document.getElementById("l1").disabled = true;
-        var v=document.getElementById("l11").disabled = true;
+        labObj[d].disabled=true;
+        labObj[t].disabled=true;
     }
-    
+
+
+/* for lab needed indexs {2,3} ,{6,7}, {10,11}*/
+
+
   }
